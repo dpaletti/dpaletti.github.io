0d896dd1f0e247063a36ba990bea282b3a409c4d
dpaletti <danielepaletti98@gmail.com>
2024-04-14 22:50:03 +0200

    Added dates to posts and projects

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 module.exports = function(eleventyConfig) {
   
@@ -9,6 +10,13 @@ module.exports = function(eleventyConfig) {
 // this is needed to automatically handle prefixing
 // gh-pages hosting needs prefixing.
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
+// format date to YYYY-mm-dd
+    eleventyConfig.addFilter("YearMonthDay", (dateString) => {
+        dateObj = new Date(dateString);
+        return dateObj.toJSON().slice(0, 10);
+    });
+
   };
   
 
