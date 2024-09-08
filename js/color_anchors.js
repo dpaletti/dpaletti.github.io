499ab867162044f83e8ccba11bef83512bbc6061
dpaletti <dpaletti@users.noreply.github.com>
2024-09-08 22:27:34 +0000

    deploy: 69a060077e1f301879f745a1d5f00dd44f6b9a46

--- js/color_anchors.js
+++
@@ -1,7 +0,0 @@
-$('a').filter(function (index) {
-                  return $(this).css("display") === "block";
-              }).each(function(index){
-                const colors = ['yellow', 'red', 'blue'];
-                const colorIndex = index % colors.length;
-                $(this).css("text-decoration-color", colors[colorIndex])
-              });
